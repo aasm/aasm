d43889d7ceaa14e52797fadaf94ae442199c5547
Konstantin Shabanov <kes.eclipse@gmail.com>
2011-01-14 18:12:10 +0600

    seems this is working solution, but refactoring needed

--- lib/aasm/i18n.rb
+++ lib/aasm/i18n.rb
@@ -1,24 +1,29 @@
 module AASM::I18n
   module InstanceMethods
     def human_state
-      _translate("attributes", aasm_current_state)
+      ancestors = self.class.ancestors.select do |x|
+        x.respond_to?(:model_name) unless x == ActiveRecord::Base
+      end
+      
+      defaults = ancestors.map do |klass|
+        human_klass = klass.model_name.respond_to?(:i18n_key) ? klass.model_name.i18n_key : klass.name.underscore
+        :"#{self.class.i18n_scope}.attributes.#{human_klass}.#{self.class.aasm_column}.#{aasm_current_state}"
+      end
+      defaults << aasm_current_state.to_s.humanize
+
+      I18n.translate(defaults.shift, :default => defaults, :raise => true)
     end
     
     def human_event_name(event)
-      _translate("events", event)
-    end
-    
-    private
-    
-    def _translate(local_scope, attribute)
-      lookuped_ancestors = self.class.ancestors.select do |x|
+      ancestors = self.class.ancestors.select do |x|
         x.respond_to?(:model_name) unless x == ActiveRecord::Base
       end
-      defaults = lookuped_ancestors.map do |klass|
-        klass_human = klass.model_name.respond_to?(:i18n_key) ? klass.model_name.i18n_key : klass.name.underscore
-        :"#{self.class.i18n_scope}.#{local_scope}.#{klass_human}.#{attribute}"
+
+      defaults = ancestors.map do |klass|
+        human_klass = klass.model_name.respond_to?(:i18n_key) ? klass.model_name.i18n_key : klass.name.underscore
+        :"#{self.class.i18n_scope}.events.#{human_klass}.#{event}"
       end
-      defaults << attribute.to_s.humanize
+      defaults << event.to_s.humanize
 
       I18n.translate(defaults.shift, :default => defaults, :raise => true)
     end
