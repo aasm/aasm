97f898e674a3547c739b177ed626810f4c377b73
Thorsten Böttger <boettger@mt7.de>
2013-05-03 23:48:12 +0200

    don't repeat yourself

--- lib/aasm.rb
+++ lib/aasm.rb
@@ -1,17 +1,18 @@
 require 'ostruct'
 
-# TODO shorten this [thorsten, 2011-12-20]
-require File.join(File.dirname(__FILE__), 'aasm', 'version')
-require File.join(File.dirname(__FILE__), 'aasm', 'errors')
-require File.join(File.dirname(__FILE__), 'aasm', 'base')
-require File.join(File.dirname(__FILE__), 'aasm', 'instance_base')
-require File.join(File.dirname(__FILE__), 'aasm', 'transition')
-require File.join(File.dirname(__FILE__), 'aasm', 'event')
-require File.join(File.dirname(__FILE__), 'aasm', 'state')
-require File.join(File.dirname(__FILE__), 'aasm', 'localizer')
-require File.join(File.dirname(__FILE__), 'aasm', 'state_machine')
-require File.join(File.dirname(__FILE__), 'aasm', 'persistence')
-require File.join(File.dirname(__FILE__), 'aasm', 'aasm')
+%w(
+    version
+    errors
+    base
+    instance_base
+    transition
+    event
+    state
+    localizer
+    state_machine
+    persistence
+    aasm
+  ).each { |file| require File.join(File.dirname(__FILE__), 'aasm', file) }
 
 # load the deprecated methods and modules
 Dir[File.join(File.dirname(__FILE__), 'aasm', 'deprecated', '*.rb')].sort.each { |f| require File.expand_path(f) }
