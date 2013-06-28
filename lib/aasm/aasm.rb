a4bc27cff0d7b7fa5ef61e517555e17b82285254
Thorsten Böttger <boettger@mt7.de>
2013-06-28 22:19:27 +0200

    added a comment to explain things

--- lib/aasm/aasm.rb
+++ lib/aasm/aasm.rb
@@ -2,7 +2,11 @@ module AASM
 
   def self.included(base) #:nodoc:
     base.extend AASM::ClassMethods
+
+    # do not overwrite existing state machines, which could have been created by
+    # inheritance, see class method inherited
     AASM::StateMachine[base] ||= AASM::StateMachine.new('')
+
     AASM::Persistence.load_persistence(base)
     super
   end
