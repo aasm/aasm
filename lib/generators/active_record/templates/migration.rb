884f3af6d7559afafa68647ff2fb76303abcc7b2
Anil Maurya <anil@joshsoftware.com>
2015-03-06 15:51:23 +0530

    Add generators to configure active_record and mongoid after install
    - Added command `rails generate aasm NAME [COLUMN_NAME]`

---
+++ lib/generators/active_record/templates/migration.rb
@@ -0,0 +1,8 @@
+class AasmCreate<%= table_name.camelize %> < ActiveRecord::Migration
+  def change
+    create_table(:<%= table_name %>) do |t|
+      t.string :<%= column_name %>
+      t.timestamps null: false 
+    end
+  end
+end
