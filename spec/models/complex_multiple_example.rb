8d0cc35547cc5ef351e0b0cc692af1de6d96c865
Thorsten Böttger <boettger@mt7.de>
2015-05-18 22:05:18 +1200

    first tests for state machines with multiple state attributes

--- spec/models/complex_multiple_example.rb
+++ spec/models/complex_multiple_example.rb
@@ -47,58 +47,59 @@ class ComplexMultipleExample
     end
   end
 
-  # aasm(:right) do
-  #   state :passive
-  #   state :pending, :initial => true, :before_enter => :make_activation_code
-  #   state :active,  :before_enter => :do_activate
-  #   state :suspended
-  #   state :deleted, :before_enter => :do_delete#, :exit => :do_undelete
-  #   state :waiting
+  aasm(:right) do
+    state :passive
+    state :pending, :initial => true, :before_enter => :make_activation_code
+    state :active,  :before_enter => :do_activate
+    state :suspended
+    state :deleted, :before_enter => :do_delete#, :exit => :do_undelete
+    state :waiting
 
-  #   event :register do
-  #     transitions :from => :passive, :to => :pending do
-  #       guard do
-  #         can_register?
-  #       end
-  #     end
-  #   end
+    event :right_register do
+      transitions :from => :passive, :to => :pending do
+        guard do
+          can_register?
+        end
+      end
+    end
 
-  #   event :activate do
-  #     transitions :from => :pending, :to => :active
-  #   end
+    event :right_activate do
+      transitions :from => :pending, :to => :active
+    end
 
-  #   event :suspend do
-  #     transitions :from => [:passive, :pending, :active], :to => :suspended
-  #   end
+    event :right_suspend do
+      transitions :from => [:passive, :pending, :active], :to => :suspended
+    end
 
-  #   event :delete do
-  #     transitions :from => [:passive, :pending, :active, :suspended], :to => :deleted
-  #   end
+    event :right_delete do
+      transitions :from => [:passive, :pending, :active, :suspended], :to => :deleted
+    end
 
-  #   # a dummy event that can never happen
-  #   event :unpassify do
-  #     transitions :from => :passive, :to => :active, :guard => Proc.new {|u| false }
-  #   end
+    # a dummy event that can never happen
+    event :right_unpassify do
+      transitions :from => :passive, :to => :active, :guard => Proc.new {|u| false }
+    end
 
-  #   event :unsuspend do
-  #     transitions :from => :suspended, :to => :active,  :guard => Proc.new { has_activated? }
-  #     transitions :from => :suspended, :to => :pending, :guard => :has_activation_code?
-  #     transitions :from => :suspended, :to => :passive
-  #   end
+    event :right_unsuspend do
+      transitions :from => :suspended, :to => :active,  :guard => Proc.new { has_activated? }
+      transitions :from => :suspended, :to => :pending, :guard => :has_activation_code?
+      transitions :from => :suspended, :to => :passive
+    end
 
-  #   event :wait do
-  #     transitions :from => :suspended, :to => :waiting, :guard => :if_polite?
-  #   end
-  # end # right
+    event :right_wait do
+      transitions :from => :suspended, :to => :waiting, :guard => :if_polite?
+    end
+  end # right
 
   def initialize
     # the AR backend uses a before_validate_on_create :aasm_ensure_initial_state
     # lets do something similar here for testing purposes.
     aasm(:left).enter_initial_state
+    aasm(:right).enter_initial_state
   end
 
   def make_activation_code
-    @activation_code = 'moo'
+    @activation_code = @activation_code ? @activation_code + '2' : '1'
   end
 
   def do_activate
