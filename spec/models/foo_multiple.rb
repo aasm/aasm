d0172e986336629e13011c0f77b070755f3ebbe8
Thorsten Böttger <boettger@mt7.de>
2015-05-26 21:21:04 +1200

    more tests for multiple state machines (callbacks, rest)

---
+++ spec/models/foo_multiple.rb
@@ -0,0 +1,35 @@
+class FooMultiple
+  include AASM
+  aasm(:left) do
+    state :open, :initial => true, :before_exit => :before_exit
+    state :closed, :before_enter => :before_enter
+    state :final
+
+    event :close, :success => :success_callback do
+      transitions :from => [:open], :to => [:closed]
+    end
+
+    event :null do
+      transitions :from => [:open], :to => [:closed, :final], :guard => :always_false
+    end
+  end
+
+  def always_false
+    false
+  end
+
+  def success_callback
+  end
+
+  def before_enter
+  end
+  def before_exit
+  end
+end
+
+class FooTwoMultiple < FooMultiple
+  include AASM
+  aasm(:left) do
+    state :foo
+  end
+end
