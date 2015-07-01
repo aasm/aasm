7c02bafcad1be2b325673eb4faaface9cbebd5ab
Thorsten Böttger <boettger@mt7.de>
2015-07-01 21:35:04 +1200

    add tests for subclassing classes with multiple state machines

--- spec/models/super_class.rb
+++ spec/models/super_class.rb
@@ -16,3 +16,31 @@ class SuperClass
     end
   end
 end
+
+class SuperClassMultiple
+  include AASM
+
+  aasm(:left) do
+    state :read
+    state :ended
+
+    event :foo do
+      transitions :to => :ended, :from => [:read]
+    end
+  end
+
+  aasm(:right) do
+    state :opened
+    state :closed
+
+    event :close do
+      transitions :to => :closed, :from => [:opened]
+    end
+  end
+
+  def update_state
+    if may_foo?
+      foo!
+    end
+  end
+end
