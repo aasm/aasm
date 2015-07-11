abcad714a9829f43894d41ac07ea36098e552c27
Thorsten Böttger <boettger@mt7.de>
2015-07-11 22:20:26 +1200

    test support for Sequel and multiple state machines per class

--- spec/unit/persistence/sequel_persistence_multiple_spec.rb
+++ spec/unit/persistence/sequel_persistence_multiple_spec.rb
@@ -16,10 +16,12 @@ describe 'sequel' do
 
       @model = Class.new(Sequel::Model(db)) do
         set_dataset(:models)
-        attr_accessor :default
         include AASM
-        aasm :column => :status
-        aasm do
+
+        attr_accessor :default
+
+        aasm :left, :column => :status
+        aasm :left do
           state :alpha, :initial => true
           state :beta
           state :gamma
@@ -40,7 +42,7 @@ describe 'sequel' do
       end
 
       it "should return the initial state when new and the aasm field is nil" do
-        expect(model.aasm.current_state).to eq(:alpha)
+        expect(model.aasm(:left).current_state).to eq(:alpha)
       end
 
       it "should save the initial state" do
@@ -50,19 +52,19 @@ describe 'sequel' do
 
       it "should return the aasm column when new and the aasm field is not nil" do
         model.status = "beta"
-        expect(model.aasm.current_state).to eq(:beta)
+        expect(model.aasm(:left).current_state).to eq(:beta)
       end
 
       it "should return the aasm column when not new and the aasm_column is not nil" do
         allow(model).to receive(:new?).and_return(false)
         model.status = "gamma"
-        expect(model.aasm.current_state).to eq(:gamma)
+        expect(model.aasm(:left).current_state).to eq(:gamma)
       end
 
       it "should allow a nil state" do
         allow(model).to receive(:new?).and_return(false)
         model.status = nil
-        expect(model.aasm.current_state).to be_nil
+        expect(model.aasm(:left).current_state).to be_nil
       end
 
       it "should not change the state if state is not loaded" do
@@ -70,7 +72,7 @@ describe 'sequel' do
         model.save
         model.class.select(:id).first.save
         model.reload
-        expect(model.aasm.current_state).to eq(:beta)
+        expect(model.aasm(:left).current_state).to eq(:beta)
       end
 
       it "should call aasm_ensure_initial_state on validation before create" do
@@ -86,33 +88,33 @@ describe 'sequel' do
 
     describe 'subclasses' do
       it "should have the same states as its parent class" do
-        expect(Class.new(@model).aasm.states).to eq(@model.aasm.states)
+        expect(Class.new(@model).aasm(:left).states).to eq(@model.aasm(:left).states)
       end
 
       it "should have the same events as its parent class" do
-        expect(Class.new(@model).aasm.events).to eq(@model.aasm.events)
+        expect(Class.new(@model).aasm(:left).events).to eq(@model.aasm(:left).events)
       end
 
       it "should have the same column as its parent even for the new dsl" do
-        expect(@model.aasm.attribute_name).to eq(:status)
-        expect(Class.new(@model).aasm.attribute_name).to eq(:status)
+        expect(@model.aasm(:left).attribute_name).to eq(:status)
+        expect(Class.new(@model).aasm(:left).attribute_name).to eq(:status)
       end
     end
 
     describe 'initial states' do
       it 'should support conditions' do
-        @model.aasm do
+        @model.aasm(:left) do
           initial_state lambda{ |m| m.default }
         end
 
-        expect(@model.new(:default => :beta).aasm.current_state).to eq(:beta)
-        expect(@model.new(:default => :gamma).aasm.current_state).to eq(:gamma)
+        expect(@model.new(:default => :beta).aasm(:left).current_state).to eq(:beta)
+        expect(@model.new(:default => :gamma).aasm(:left).current_state).to eq(:gamma)
       end
     end
 
   rescue LoadError
     puts "------------------------------------------------------------------------"
-    puts "Not running Sequel specs because sequel gem is not installed!!!"
+    puts "Not running Sequel multiple-specs because sequel gem is not installed!!!"
     puts "------------------------------------------------------------------------"
   end
 end
