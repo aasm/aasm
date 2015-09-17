12227e7525f558485b8d89f9bfcb4736c71c97c7
Thorsten Böttger <boettger@mt7.de>
2015-09-17 18:05:30 +1200

    add test for the Job example from the README (in Usage)

---
+++ spec/unit/readme_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe 'testing the README examples' do
+
+  it 'Usage' do
+    class Job
+      include AASM
+
+      aasm do
+        state :sleeping, :initial => true
+        state :running
+        state :cleaning
+
+        event :run do
+          transitions :from => :sleeping, :to => :running
+        end
+
+        event :clean do
+          transitions :from => :running, :to => :cleaning
+        end
+
+        event :sleep do
+          transitions :from => [:running, :cleaning], :to => :sleeping
+        end
+      end
+
+    end
+
+    job = Job.new
+
+    expect(job.sleeping?).to eql true
+    expect(job.may_run?).to eql true
+
+    job.run
+
+    expect(job.running?).to eql true
+    expect(job.sleeping?).to eql false
+    expect(job.may_run?).to eql false
+
+    expect { job.run }.to raise_error(AASM::InvalidTransition)
+  end
+
+end
