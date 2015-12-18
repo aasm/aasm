e29fb8b66f9932ff53139c4cb26c704d9a785153
HoyaBoya <christopher.sun@gmail.com>
2015-12-18 17:36:48 -0500

    better tests(?)

--- spec/unit/simple_custom_example_spec.rb
+++ spec/unit/simple_custom_example_spec.rb
@@ -1,22 +1,39 @@
 require 'spec_helper'
 
-describe 'state machine' do
-  let(:simple_custom) { SimpleCustomExample.new }
+describe 'Custom AASM::Base' do
+  context 'when aasm_with invoked with SimpleCustomExample' do
+    let(:simple_custom) { SimpleCustomExample.new }
 
-  subject do
-    simple_custom.fill_out!
-    simple_custom.authorise
+    subject do
+      simple_custom.fill_out!
+      simple_custom.authorise
+    end
+
+    it 'has invoked authorizable?' do
+      expect { subject }.to change { simple_custom.authorizable_called }.from(nil).to(true)
+    end
+
+    it 'has invoked fillable?' do
+      expect { subject }.to change { simple_custom.fillable_called }.from(nil).to(true)
+    end
+
+    it 'has two transition counts' do
+      expect { subject }.to change { simple_custom.transition_count }.from(nil).to(2)
+    end
   end
 
-  it 'has invoked authorizable?' do
-    expect { subject }.to change { simple_custom.authorizable_called }.from(nil).to(true)
-  end
+  context 'when aasm_with invoked with non AASM::Base' do
+    subject do
+      class SomeModel
+        include AASM
 
-  it 'has invoked fillable?' do
-    expect { subject }.to change { simple_custom.fillable_called }.from(nil).to(true)
-  end
+        aasm_with String do
+        end
+      end
+    end
 
-  it 'has two transition counts' do
-    expect { subject }.to change { simple_custom.transition_count }.from(nil).to(2)
+    it 'should raise an ArgumentError' do
+      expect { subject }.to raise_error(ArgumentError, 'The class String must inherit from AASM::Base!')
+    end
   end
 end
