ada46aaee65b44d0cf253cb713a41c6b693b43dc
long-long-float <niinikazuki@yahoo.co.jp>
2016-03-10 13:52:24 +0900

    fix migration name

--- lib/generators/active_record/templates/migration_existing.rb
+++ lib/generators/active_record/templates/migration_existing.rb
@@ -1,4 +1,4 @@
-class AddAASMTo<%= table_name.camelize %> < ActiveRecord::Migration
+class AddAasm<%= column_name.camelize %>To<%= table_name.camelize %> < ActiveRecord::Migration
   def self.up
     add_column :<%= table_name %>, :<%= column_name %>, :string
   end
