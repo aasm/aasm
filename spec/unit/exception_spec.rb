b8918956171eb03adb317bb6bb7961d6fa68c381
Phil Pirozhkov <pirjsuka@gmail.com>
2016-03-15 22:54:35 +0300

    Eager error message

---
+++ spec/unit/exception_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe AASM::InvalidTransition do
+  it 'should not be lazy detecting originating state' do
+    process = ProcessWithNewDsl.new
+    expect { process.stop! }.to raise_error do |err|
+      process.start
+      expect(err.message).to eql("Event 'stop' cannot transition from 'sleeping'. ")
+    end
+  end
+end
