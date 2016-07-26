27af316f5b3edf8340085ba8ba380b56b16c1e5d
Farrukh Abdulkadyrov <farrukhabdul@gmail.com>
2016-07-26 17:49:08 -0400

    support Minitest expectation must_have_state

---
+++ lib/aasm/minitest_spec/have_state.rb
@@ -0,0 +1,16 @@
+module Minitest::Assertions
+  def assert_have_state(object, state, **options)
+    state_machine_name = options[:on] || :default
+    assert object.aasm(state_machine_name).current_state == state,
+         "Expected that :#{object.aasm(state_machine_name).current_state} would be :#{state} (on :#{state_machine_name})"
+  end
+
+  def refute_have_state(object, state, **options)
+    state_machine_name = options[:on] || :default
+    assert object.aasm(state_machine_name).current_state == state,
+         "Expected that :#{object.aasm(state_machine_name).current_state} would be :#{state} (on :#{state_machine_name})"
+  end
+
+  Object.infect_an_assertion :assert_have_state, :must_have_state, :do_not_flip
+  Object.infect_an_assertion :refute_have_state, :wont_have_state, :do_not_flip
+end
