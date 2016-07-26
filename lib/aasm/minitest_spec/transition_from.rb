45bd29edf9b48d1ab7bb71e18c30a5c454d3b8d7
Farrukh Abdulkadyrov <farrukhabdul@gmail.com>
2016-07-26 17:49:46 -0400

    Support Minitest expectation must_transition_to

---
+++ lib/aasm/minitest_spec/transition_from.rb
@@ -0,0 +1,23 @@
+module MiniTest::Assertions
+  def assert_transitions_from(object, from_state, *args, **options)
+    options[:on] ||= :default
+    assert _transitions_from?(object, from_state, args, options),
+          "Expected transition state to :#{options[:to]} from :#{from_state} on event :#{options[:on_event]}, (on :#{options[:on]})"
+  end
+
+  def refute_transitions_from(object, from_state, *args, **options)
+    options[:on] ||= :default
+    refute _transitions_from?(object, from_state, args, options),
+          "Expected transition state to :#{options[:to]} from :#{from_state} on event :#{options[:on_event]}, (on :#{options[:on]})"
+  end
+
+  def _transitions_from?(object, from_state, *args, **options)
+    state_machine_name = options[:on] || :default
+    object.aasm(state_machine_name).current_state = from_state.to_sym
+    object.send(options[:on_event], *args)
+    options[:to].to_sym == object.aasm(state_machine_name).current_state
+  end
+
+  Object.infect_an_assertion :assert_transitions_from, :must_transition_from, :do_not_flip
+  Object.infect_an_assertion :refute_transitions_from, :wont_transition_from, :do_not_flip
+end
