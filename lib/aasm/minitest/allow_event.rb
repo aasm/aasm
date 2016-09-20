c9482de28057cf8471fabfe2b722eb9af09b6244
Farrukh Abdulkadyrov <farrukhabdul@gmail.com>
2016-09-20 15:04:49 -0400

    ruby backward compatibility

--- lib/aasm/minitest/allow_event.rb
+++ lib/aasm/minitest/allow_event.rb
@@ -1,12 +1,12 @@
 module Minitest::Assertions
-  def assert_event_allowed(object, event, **options)
-    state_machine_name = options[:on] || :default
+  def assert_event_allowed(object, event, options = {})
+    state_machine_name = options.fetch(:on, :default)
     assert object.aasm(state_machine_name).may_fire_event?(event),
           "Expected that the event :#{event} would be allowed (on :#{state_machine_name})"
   end
 
-  def refute_event_allowed(object, event, **options)
-    state_machine_name = options[:on] || :default
+  def refute_event_allowed(object, event, options = {})
+    state_machine_name = options.fetch(:on, :default)
     refute object.aasm(state_machine_name).may_fire_event?(event),
           "Expected that the event :#{event} would not be allowed (on :#{state_machine_name})"
   end
--- lib/aasm/minitest/allow_transition_to.rb
+++ lib/aasm/minitest/allow_transition_to.rb
@@ -1,12 +1,12 @@
 module Minitest::Assertions
-  def assert_transition_to_allowed(object, to_state, *args, **options)
-    state_machine_name = options[:on] || :default
+  def assert_transition_to_allowed(object, to_state, options = {})
+    state_machine_name = options.fetch(:on, :default)
     assert object.aasm(state_machine_name).states(permitted: true).include?(to_state),
            "Expected that the state :#{to_state} would be reachable (on :#{state_machine_name})"
   end
 
-  def refute_transition_to_allowed(object, to_state, *args, **options)
-    state_machine_name = options[:on] || :default
+  def refute_transition_to_allowed(object, to_state, options = {})
+    state_machine_name = options.fetch(:on, :default)
     refute object.aasm(state_machine_name).states(permitted: true).include?(to_state),
            "Expected that the state :#{to_state} would be reachable (on :#{state_machine_name})"
   end
