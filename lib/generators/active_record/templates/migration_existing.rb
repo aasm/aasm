1ef5c17c4199af16626bdd407fd4e96bd0fe8a04
Pablo Acuna <pabloacuna88@gmail.com>
2017-05-27 11:46:14 -0400

    add current Rails version to migration

--- lib/generators/active_record/templates/migration_existing.rb
+++ lib/generators/active_record/templates/migration_existing.rb
@@ -1,4 +1,4 @@
-class Add<%= column_name.camelize %>To<%= table_name.camelize %> < ActiveRecord::Migration
+class Add<%= column_name.camelize %>To<%= table_name.camelize %> < ActiveRecord::Migration[<%= ActiveRecord::Migration.current_version %>]
   def change
     add_column :<%= table_name %>, :<%= column_name %>, :string
   end
