8cbd7dd38f94b26efca85c2c3c94467652cde390
Pablo Acuna <pabloacuna88@gmail.com>
2017-05-27 11:45:30 -0400

    add current rails version to migration

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,4 +1,4 @@
-class AASMCreate<%= table_name.camelize %> < ActiveRecord::Migration
+class AASMCreate<%= table_name.camelize %> < ActiveRecord::Migration[<%= ActiveRecord::Migration.current_version %>]
   def change
     create_table(:<%= table_name %>) do |t|
       t.string :<%= column_name %>
