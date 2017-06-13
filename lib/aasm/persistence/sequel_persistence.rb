0c4a25a8d49975f0c94ac071823cd5b1ab261752
Aryk Grosz <aryk.grosz@gmail.com>
2017-06-13 10:00:34 -0700

    Add Sequel transactions and locking
     * Add aasm features of ActiveRecord to Sequel! :)
     * All specs pass for sequel, activerecord, and mongoid.
     * Refactor Sequel models to be namespaced under “Sequel”. We
       should do something similar for ActiveRecord in the specs
       to avoid collisions.
    
    Addresses #474
    
    (cherry picked from commit df97ce754f47864abbcb837227da5c4d0ff77289)

--- lib/aasm/persistence/sequel_persistence.rb
+++ lib/aasm/persistence/sequel_persistence.rb
@@ -1,8 +1,10 @@
+require 'aasm/persistence/orm'
 module AASM
   module Persistence
     module SequelPersistence
       def self.included(base)
         base.send(:include, AASM::Persistence::Base)
+        base.send(:include, AASM::Persistence::ORM)
         base.send(:include, AASM::Persistence::SequelPersistence::InstanceMethods)
       end
 
@@ -17,40 +19,40 @@ module AASM
           super
         end
 
-        # Returns the value of the aasm.attribute_name - called from <tt>aasm.current_state</tt>
-        #
-        # If it's a new record, and the aasm state column is blank it returns the initial state
-        #
-        #   class Foo < Sequel::Model
-        #     include AASM
-        #     aasm :column => :status do
-        #       state :opened
-        #       state :closed
-        #     end
-        #   end
-        #
-        #   foo = Foo.new
-        #   foo.current_state # => :opened
-        #   foo.close
-        #   foo.current_state # => :closed
-        #
-        #   foo = Foo[1]
-        #   foo.current_state # => :opened
-        #   foo.aasm_state = nil
-        #   foo.current_state # => nil
-        #
-        # NOTE: intended to be called from an event
-        #
-        # This allows for nil aasm states - be sure to add validation to your model
-        def aasm_read_state(name=:default)
-          state = send(self.class.aasm(name).attribute_name)
-          if new? && state.to_s.strip.empty?
-            aasm(name).determine_state_name(self.class.aasm(name).initial_state)
-          elsif state.nil?
-            nil
-          else
-            state.to_sym
+        def aasm_raise_invalid_record
+          raise Sequel::ValidationFailed.new(self)
+        end
+
+        def aasm_new_record?
+          new?
+        end
+
+        # Returns nil if fails silently
+        # http://sequel.jeremyevans.net/rdoc/classes/Sequel/Model/InstanceMethods.html#method-i-save
+        def aasm_save
+          !save(raise_on_failure: false).nil?
+        end
+
+        def aasm_read_attribute(name)
+          send(name)
+        end
+
+        def aasm_write_attribute(name, value)
+          send("#{name}=", value)
+        end
+
+        def aasm_transaction(requires_new, requires_lock)
+          self.class.db.transaction(savepoint: requires_new) do
+            if requires_lock
+              # http://sequel.jeremyevans.net/rdoc/classes/Sequel/Model/InstanceMethods.html#method-i-lock-21
+              requires_lock.is_a?(String) ? lock!(requires_lock) : lock!
+            end
+            yield
           end
+        end
+
+        def aasm_update_column(attribute_name, value)
+          this.update(attribute_name => value)
         end
 
         # Ensures that if the aasm_state column is nil and the record is new
@@ -72,39 +74,10 @@ module AASM
           AASM::StateMachineStore.fetch(self.class, true).machine_names.each do |state_machine_name|
             aasm(state_machine_name).enter_initial_state if
               (new? || values.key?(self.class.aasm(state_machine_name).attribute_name)) &&
-              send(self.class.aasm(state_machine_name).attribute_name).to_s.strip.empty?
+                send(self.class.aasm(state_machine_name).attribute_name).to_s.strip.empty?
           end
         end
 
-        # Writes <tt>state</tt> to the state column and persists it to the database
-        #
-        #   foo = Foo[1]
-        #   foo.aasm.current_state # => :opened
-        #   foo.close!
-        #   foo.aasm.current_state # => :closed
-        #   Foo[1].aasm.current_state # => :closed
-        #
-        # NOTE: intended to be called from an event
-        def aasm_write_state state, name=:default
-          aasm_column = self.class.aasm(name).attribute_name
-          update_fields({aasm_column => state.to_s}, [aasm_column], missing: :skip)
-        end
-
-        # Writes <tt>state</tt> to the state column, but does not persist it to the database
-        #
-        #   foo = Foo[1]
-        #   foo.aasm.current_state # => :opened
-        #   foo.close
-        #   foo.aasm.current_state # => :closed
-        #   Foo[1].aasm.current_state # => :opened
-        #   foo.save
-        #   foo.aasm.current_state # => :closed
-        #   Foo[1].aasm.current_state # => :closed
-        #
-        # NOTE: intended to be called from an event
-        def aasm_write_state_without_persistence state, name=:default
-          send("#{self.class.aasm(name).attribute_name}=", state.to_s)
-        end
       end
     end
   end
