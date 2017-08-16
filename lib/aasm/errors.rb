5e29fe5de4fbb582d19f08b2fa926f5a54467c44
Oleksandr Avoyants <shhavel@gmail.com>
2017-08-16 13:47:24 +0300

    Remove trailing space from error message

--- lib/aasm/errors.rb
+++ lib/aasm/errors.rb
@@ -7,11 +7,11 @@ module AASM
 
     def initialize(object, event_name, state_machine_name, failures = [])
       @object, @event_name, @originating_state, @failures = object, event_name, object.aasm(state_machine_name).current_state, failures
-      super("Event '#{event_name}' cannot transition from '#{originating_state}'. #{reasoning}")
+      super("Event '#{event_name}' cannot transition from '#{originating_state}'.#{reasoning}")
     end
 
     def reasoning
-      "Failed callback(s): #{failures}." unless failures.empty?
+      " Failed callback(s): #{failures}." unless failures.empty?
     end
   end
 
