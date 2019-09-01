6400f5bc6d739d8f4c0b72a441e1bf508e833ac5
Anil Maurya <anil@joshsoftware.com>
2019-09-01 20:08:48 +0530

    Setup codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
 $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib')))
 require 'aasm'
