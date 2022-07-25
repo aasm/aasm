280f9b8e3ad82571607a9d75f6b08f179be031bc
Norman Clarke <norman@njclarke.com>
2022-07-25 12:59:58 -0300

    Return early from method if event found

--- lib/aasm/instance_base.rb
+++ lib/aasm/instance_base.rb
@@ -135,8 +135,9 @@ module AASM
 
     def event_exists?(event_name, bang = false)
       event = @instance.class.aasm(@name).state_machine.events[event_name.to_sym]
-      event_error = bang ? "#{event_name}!" : event_name
+      return true if event
 
+      event_error = bang ? "#{event_name}!" : event_name
       raise AASM::UndefinedState, "State :#{event_error} doesn't exist" if event.nil?
     end
   end
