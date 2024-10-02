6daf5c3768f24845bab739ac6d29768f1a6f1464
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2024-10-02 10:53:55 +0900

    Fixes an message when defining an enum not backed by a database column

--- spec/unit/persistence/active_record_persistence_multiple_spec.rb
+++ spec/unit/persistence/active_record_persistence_multiple_spec.rb
@@ -123,7 +123,9 @@ if defined?(ActiveRecord)
           let(:multiple_with_enum_without_column) {MultipleWithEnumWithoutColumn.new}
 
           it "should raise an error for transitions" do
-            if ActiveRecord.gem_version >= Gem::Version.new('7.1.0')
+            if ActiveRecord.gem_version >= Gem::Version.new('7.2.0')
+              expect{multiple_with_enum_without_column.send(:view, :left)}.to raise_error(RuntimeError, /Unknown enum attribute 'status' for MultipleWithEnumWithoutColumn/)
+            elsif ActiveRecord.gem_version >= Gem::Version.new('7.1.0')
               expect{multiple_with_enum_without_column.send(:view, :left)}.to raise_error(RuntimeError, /Undeclared attribute type for enum 'status'/)
             else
               expect{multiple_with_enum_without_column.send(:view, :left)}.to raise_error(NoMethodError, /undefined method .status./)
